5df9738c63471870f73d91e06ef7dd55449bfe43
Madalina Moldoveanu <patrascu.madalina.gabriela@gmail.com>
2024-03-20 18:08:08 +0200

    1

--- curs1/js/carusel.js
+++
@@ -1,8 +0,0 @@
-var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel, {
-    interval: 2000,
-    wrap: true,
-    pause: 'hover', //false
-    keyboard: true,
-    touch: true
-})
