27cf08c3096ba53c0ef3e0c83be8c6a25f87e712
Madalina Moldoveanu <patrascu.madalina.gabriela@gmail.com>
2024-04-05 18:43:10 +0300

    1

---
+++ curs 4/cvtemplate_v4/src/index.js
@@ -0,0 +1,21 @@
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+// Import https://github.com/VincentGarreau/particles.js
+import particles from 'particles.js'
+import './js/particles.js'
+
+
+// Import https://github.com/mattboldt/typed.js/
+// import Typed from 'typed.js';
+import '../node_modules/typed.js/dist/typed.umd.js'
+
+// import GLightbox from 'glightbox';
+import '../node_modules/glightbox/dist/js/glightbox.min.js'
+
+import './js/main.js';
+
+import "./js/skill.js";
